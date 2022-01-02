aeea05cbdb7f78dc41af18c7b7c28d8b2c22d294
Jack <jackohara96@gmail.com>
2022-01-02 16:32:37 -0500

    Final commit

--- tictactoe.js
+++ tictactoe.js
@@ -1,136 +1,108 @@
-//left off here at 7:08 https://www.youtube.com/watch?v=B3pmT7Cpi24
-//const GameBoardObj = (function () {
-    let gameBoard = new Array(9).fill("null");
-    console.log(gameBoard);
-    
-//})();
+//Gameboard
+let gameBoard = new Array(9).fill("null");
 
-
-//Reset Button
+//Reset button
 document.getElementById("reset").addEventListener('click', resetBtn);
 
-function resetBtn(){
-    
-    const x= document.querySelectorAll(".board__tile");
-    for (let i=0;i<x.length;i++){
-        x[i].innerHTML="";
+function resetBtn() {
+
+    const x = document.querySelectorAll(".board__tile");
+    for (let i = 0; i < x.length; i++) {
+        x[i].innerHTML = "";
     }
     delete lll;
     lll();
-    gameBoard=["null","null","null","null","null","null","null","null","null",];
-    //problem wont update display after you press reset
-    document.querySelector(".display").innerHTML= `Player <span class="display-player">${currentPlayer}</span>'s turn`;
+    gameBoard = ["null", "null", "null", "null", "null", "null", "null", "null", "null",];
+    document.querySelector(".display").innerHTML = `Player <span class="display-player">${currentPlayer}</span>'s turn`;
     document.querySelector(".board").classList.remove("unclick");
-    for(let v=0;v<lis_array.length;v++){
-        
-            lis_array[v].classList.remove("winner"); 
-        
+    for (let v = 0; v < lis_array.length; v++) {
+
+        lis_array[v].classList.remove("winner");
+
+    }
+    const gif = document.getElementById('gif');
+    gif.src = "";
+}
+
+let currentPlayer = 'X';
+
+function switchTurns() {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+        document.querySelector('.display-player').innerHTML = currentPlayer;
+    } else {
+        currentPlayer = 'X';
+        document.querySelector('.display-player').innerHTML = currentPlayer;
     }
 }
-//Other important functions
-//maybe get board to match up with array so you can use the array to check a winner
 
-//Mark board and update array
-//document.querySelectorAll("[data-index]").forEach(tile => {
-  // this.addEventListener('click', addMarks(tile.dataset.index)) 
-//});
-  
-//problem below:
-let currentPlayer= 'X';
-
-
-
-function switchTurns(){
- if (currentPlayer==='X') {
-     currentPlayer='O';
-     document.querySelector('.display-player').innerHTML= currentPlayer;
- } else {
-     currentPlayer='X';
-     document.querySelector('.display-player').innerHTML= currentPlayer;
- }
-}
-//update display
-//document.querySelector('.display-player').innerHTML= currentPlayer;
-
-
-let lis= document.querySelectorAll("[data-index]");
+let lis = document.querySelectorAll("[data-index]");
 
 let lis_array = Array.from(lis);
 
-function lll() {lis.forEach(function(elem) {
-    elem.addEventListener('click', function(e) {
-        if(currentPlayer==='X'){
-            let i= e.target.dataset.index;
-            elem.innerHTML= currentPlayer; 
-            //Use to update array:
-            
-            gameBoard[i]=currentPlayer;
-            checkWinner();
-            switchTurns();
-        } else if (currentPlayer==='O'){
-            let i= e.target.dataset.index;
-            elem.innerHTML= currentPlayer;
-            gameBoard[i]=currentPlayer;
-            checkWinner();
-            switchTurns();
-        }
-        console.log("clicked")
-}, { once: true });
-});
+function lll() {
+    lis.forEach(function (elem) {
+        elem.addEventListener('click', function (e) {
+            if (currentPlayer === 'X') {
+                let i = e.target.dataset.index;
+                elem.innerHTML = currentPlayer;
+                //Use to update array:
+                gameBoard[i] = currentPlayer;
+                checkWinner();
+                switchTurns();
+            } else if (currentPlayer === 'O') {
+                let i = e.target.dataset.index;
+                elem.innerHTML = currentPlayer;
+                gameBoard[i] = currentPlayer;
+                checkWinner();
+                switchTurns();
+            }
+            console.log("clicked")
+        }, { once: true });
+    });
 }
 lll();
-/*function addMarks(i){
-    //update array and get x or o to show up on board when yoy uclick
-// data-index
-    console.log(`tile clicked: ${i}`);
-}*/
-//problem above
 
 let winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 
-function checkWinner(){
-    //if (if the values at these indexes are equal thereś a winner;gameBoard contains all Xs or all Os at the indexes in winningCombinations theres a winner)
-for (let j=0;j<winningCombinations.length;j++){
-    for (let k=0;k<gameBoard.length;k++){
-    
-        if(gameBoard[winningCombinations[j][0]]==="null"||gameBoard[winningCombinations[j][1]]==="null"||gameBoard[winningCombinations[j][2]]==="null"){
-    console.log('No winner');
-        } else if(gameBoard[winningCombinations[j][0]]===gameBoard[winningCombinations[j][1]] &&gameBoard[winningCombinations[j][0]]===gameBoard[winningCombinations[j][2]]){
-        console.log('Winner')
-        document.querySelector(".display").innerHTML=`Player ${currentPlayer} wins!`;
-        document.querySelector(".board").classList.add("unclick");
-            //loop through datasets if equal current player then turn green
-    
-            for(let v=0;v<lis_array.length;v++){
-                if(lis_array[v].textContent===currentPlayer){
-                    lis_array[v].classList.add("winner"); 
+function checkWinner() {
+
+    for (let j = 0; j < winningCombinations.length; j++) {
+        for (let k = 0; k < gameBoard.length; k++) {
+
+            if (gameBoard[winningCombinations[j][0]] === "null" || gameBoard[winningCombinations[j][1]] === "null" || gameBoard[winningCombinations[j][2]] === "null") {
+                console.log('No winner');
+            } else if (gameBoard[winningCombinations[j][0]] === gameBoard[winningCombinations[j][1]] && gameBoard[winningCombinations[j][0]] === gameBoard[winningCombinations[j][2]]) {
+                console.log('Winner')
+                const audio = new Audio('SMALL_CROWD_APPLAUSE-Yannick_Lemieux-1268806408.mp3')
+                audio.play();
+                const gif = document.getElementById('gif');
+                async function getGif() {
+                    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=WiJMmFWVtpn3rFw3Irj2fUDat4U4xFI1&s=victory', { mode: 'cors' })
+                    const gifData = await response.json();
+                    gif.src = gifData.data.images.original.url;
                 }
+                getGif();
+
+                document.querySelector(".display").innerHTML = `Player ${currentPlayer} wins!`;
+                document.querySelector(".board").classList.add("unclick");
+                //loop through datasets if equal current player then turn green
+                for (let v = 0; v < lis_array.length; v++) {
+                    if (lis_array[v].textContent === currentPlayer) {
+                        lis_array[v].classList.add("winner");
+                    }
+                }
+
             }
-       //lis[gameBoard[winningCombinations[j][0]]];
-        //gameBoard[winningCombinations[j][1]];
-        //gameBoard[winningCombinations[j][2]];
-}
-    //for (let i=0;i<gameBoard.length;i++){
-
-    //}
-}
-}
-//return winningCombinations.some(combination => {
-  //  return combination.every(index => {
-    //    return lis[index].classList.contains(currentPlayer);
-  //  })
-//})
-//}
-
-//if(checkWinner(currentPlayer)){
-  //  console.log('winner')
+        }
+    }
 }
